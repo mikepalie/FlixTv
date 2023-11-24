9dee2bdca3a35ffb1fa817779a38191d4a88473a
mikepalie <michalis.palierakis@gmail.com>
2023-11-24 21:17:12 +0200

    Add project files.

---
+++ FlixTv/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FlixTv.Startup))]
+namespace FlixTv
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
