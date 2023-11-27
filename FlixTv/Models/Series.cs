4c0616dea44d6c1e99ebf2111639d1188e0aca89
mikepalie <michalis.palierakis@gmail.com>
2023-11-27 23:49:43 +0200

    models

---
+++ FlixTv/Models/Series.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlixTv.Models
+{
+    public class Series
+    {
+        public int SeriesId { get; set; }
+        public string Title { get; set; }
+        public int ProductionYear { get; set; }
+        public int NumberOfEpisode { get; set; }
+        public int Season { get; set; }
+        public string Casting { get; set; }
+        public double Rating { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+
+    }
+}
