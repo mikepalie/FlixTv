07e1a677cf086935bb66b7cf6b3297c8e7183424
mikepalie <michalis.palierakis@gmail.com>
2023-12-08 18:03:00 +0200

    filteringA

--- FlixTv/Models/Movie.cs
+++ FlixTv/Models/Movie.cs
@@ -8,8 +8,6 @@ namespace FlixTv.Models
     public class Movie
     {
         public string MovieId { get; set; }
-        public string Title { get; set; }
         public string Synopsis { get; set; }
-        public string ImageUrl { get; set; }
     }
 }
