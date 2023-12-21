aa68c1c489ccc954cc1f8b5565db7e7f132ce67e
mikepalie <michalis.palierakis@gmail.com>
2023-12-21 19:45:11 +0200

    loginComplete

--- FlixTv/Models/AccountViewModels.cs
+++ FlixTv/Models/AccountViewModels.cs
@@ -50,7 +50,7 @@ namespace FlixTv.Models
     {
         [Required]
         [Display(Name = "Email")]
-        [EmailAddress]
+        //[EmailAddress]
         public string Email { get; set; }
 
         [Required]
@@ -74,12 +74,6 @@ namespace FlixTv.Models
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Text)]
-        [Display(Name = "Username")]
-        public string Username { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
